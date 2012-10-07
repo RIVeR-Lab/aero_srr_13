9ff256eb188203138fd490efb0bb4e5e612c77e1
apanzica <apanzica@wpi.edu>
2012-10-07 17:39:59 -0400

    Updated to grab the parameters for communicating with DriveManager

--- oryx_drive_controller/src/nodes/oryx_drive_controller.cpp
+++ oryx_drive_controller/src/nodes/oryx_drive_controller.cpp
@@ -11,11 +11,14 @@
 #include "OryxDriveControllerConfig.h"
 
 
-//Used by ROS to actually create the node. Could theoretically spawn more than one server
+///Used by ROS to create the node.
 int main(int argc, char** argv)
 {
-	std::string v_action_topic;
-	std::string t_action_topic;
+	std::string v_action_topic;	///String containing the topic name for velocity commands
+	std::string t_action_topic;	///String containing the topic name for translate commands
+	std::string v_drive_topic;	///String containing the topic name for sending wheel velocities to the DriveManager node
+	std::string s_drive_topic;	///String containing the topic name for sending swerve positions to the DriveManager node
+	std::string c_drive_topic;	///String containing the topic name for polling the DriveManager node on its capabilities
 
 	ros::init(argc, argv, "oryx_drive_controller");
 	//Get a private node handle to parse command line arguments
@@ -23,15 +26,24 @@ int main(int argc, char** argv)
 	ROS_INFO("Oryx Drive Controller Version: %d.%d.%d", oryx_drive_controller_VERSION_MAJOR, oryx_drive_controller_VERSION_MINOR, oryx_drive_controller_VERSION_BUILD);
 	//Just a check to make sure the usage was correct
 	if(argc < 1){
-		ROS_INFO("Usage: oryx_drive_controller _v_action_topic:=string _t_action_topic:=string");
+		ROS_INFO("Usage: oryx_drive_controller velocity_command_topic translate_command_topic drive_velocity_topic drive_swerve_topic drive_capability_topic");
 		return 1;
 	}else{
-		param_nh.getParam("velocity_command_topic", v_action_topic);
-		param_nh.getParam("translate_command_topic", t_action_topic);
+		//Grab the topic parameters
+		param_nh.getParam("velocity_command_topic",		v_action_topic);
+		param_nh.getParam("translate_command_topic",	t_action_topic);
+		param_nh.getParam("drive_velocity_topic",		v_drive_topic);
+		param_nh.getParam("drive_swerve_topic",			s_drive_topic);
+		param_nh.getParam("drive_capability_topic",		c_drive_topic);
 	}
 
-	ROS_INFO("Got Velocity Action Topic Name: <%s>", v_action_topic.c_str());
-	ROS_INFO("Got Translate Action Topic Name: <%s>", t_action_topic.c_str());
+	//Print out recieved topics
+	ROS_DEBUG("Got Velocity Action Topic Name: <%s>",		v_action_topic.c_str());
+	ROS_DEBUG("Got Translate Action Topic Name: <%s>",		t_action_topic.c_str());
+	ROS_DEBUG("Got DriveManager Velocity Topic Name: <%s>",	v_drive_topic.c_str());
+	ROS_DEBUG("Got DriveManager Swerve Topic Name: <%s>",	s_drive_topic.c_str());
+	ROS_DEBUG("Got DriveManager Capability Topic Name: <%s>",	c_drive_topic.c_str());
+
 	//Spawn the servers
 	ROS_INFO("Starting Up Oryx Drive Controller...");
 	VelocityControlServer v_server(v_action_topic);
