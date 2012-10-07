e63e31815d5f4ffd48b36c068f23c92e2bd8049e
apanzica <apanzica@wpi.edu>
2012-10-07 19:35:39 -0400

    Updated documentation

--- oryx_drive_controller/src/nodes/oryx_drive_controller.cpp
+++ oryx_drive_controller/src/nodes/oryx_drive_controller.cpp
@@ -11,8 +11,27 @@
 #include "TranslateControlServer.h"
 #include "OryxDriveControllerConfig.h"
 
+/**
+ * @brief Low level velocity-arc based drive controller
+ *
+ * Implements a velocity-control based controller which is capable of performing arc based motions. There are seperate
+ * control modes for operation based on the capabilities of the base platform:
+ * *Without Swerve Capabilities
+ *  -Standard Tank Steer Velocity-Arc
+ * *With Swerve Capabilities
+ *  -Smooth Velocity-Arc
+ *  -Translation
+ */
 class DriveController{
 public:
+	/**
+	 * Creates a new DriveController with standard initialization parameters
+	 * @param drive_velocity_topic		The topic name to publish wheel velocity messages to
+	 * @param drive_swerve_topic		The topic name to publish swerve position messages to
+	 * @param drive_capabilities_topic	The topic name to poll for DriveManager capabilities
+	 * @param baseLength				The length of the base platform from front wheel center to rear wheel center
+	 * @param baseWidth					The width of the base platform from left wheel center to right wheel center
+	 */
 	DriveController(std::string drive_velocity_topic,
 					std::string drive_swerve_topic,
 					std::string drive_capabilities_topic,
@@ -30,11 +49,11 @@ public:
 		ROS_DEBUG("Got a platform size of <L=%f,W=%f>",baseLength, baseWidth);
 	}
 private:
-	double baseLength;
-	double baseWidth;
-	ros::NodeHandle nh;
-	ros::Publisher velocity_pub;
-	ros::Publisher swerve_pub;
+	double baseLength;				///The length of the platform
+	double baseWidth;				///The width of the platform
+	ros::NodeHandle nh;				///Node handle into the ROS system
+	ros::Publisher velocity_pub;	///Publisher for sending velocity messages to DriveManager
+	ros::Publisher swerve_pub;		///Publisher for sending swerve message to DriveManager
 };
 
 ///Used by ROS to create the node.
