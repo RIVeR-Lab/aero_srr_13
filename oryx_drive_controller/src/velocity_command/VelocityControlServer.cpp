a24e55d3e3b0ee887fc33fe62ddc9cf44a98e380
apanzica <apanzica@wpi.edu>
2012-10-10 13:30:14 -0400

    Fleshed out the excecute callback so that it now sends a VelocityArc message to the controller, and then returns success to its client

--- oryx_drive_controller/src/velocity_command/VelocityControlServer.cpp
+++ oryx_drive_controller/src/velocity_command/VelocityControlServer.cpp
@@ -18,4 +18,19 @@ action_name(action_name){
 
 void VelocityControlServer::executeCB(const oryx_drive_controller::VelocityCommandGoalConstPtr& goal){
 	ROS_INFO("Got Goal <R=%f, V=%f> on <%s>", goal->radius,goal->velocity, this->action_name.c_str());
+
+	//Create a new message to send to the controller
+	oryx_drive_controller::VelocityArcPtr msg(new oryx_drive_controller::VelocityArc);
+
+	//Build the message
+	msg->radius = goal->radius;
+	msg->velocity = goal->velocity;
+
+	//Publish the message
+	this->vel_pub.publish(msg);
+
+	this->result.capable = true;
+	this->result.success = true;
+	//set success
+	this->as.setSucceeded(this->result, "I successfully set the new Velocity Arc goal");
 }
