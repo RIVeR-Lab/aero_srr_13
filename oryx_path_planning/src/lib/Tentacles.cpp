81525582c3937f9a78f9e2ccf87c499f211c6271
apanzica <apanzica@wpi.edu>
2012-10-11 17:42:57 -0400

    Fleshed out methods for SpeedSet

--- oryx_path_planning/src/lib/Tentacles.cpp
+++ oryx_path_planning/src/lib/Tentacles.cpp
@@ -11,7 +11,24 @@ namespace oryx_path_planner{
 //***************************** TENTACLE *********************************//
 
 //***************************** SPEED SET *********************************//
+/**
+ * This constructor just makes an empty set of tentacles
+ */
+SpeedSet::SpeedSet(){
+	this->tentacles();
+}
 
+SpeedSet::SpeedSet(double expFact, double seedRad, int numTent, double resolution, double xDim, double yDim, double velocity){
+	ROS_INFO("Generating a Speed Set with the Parameters <SRad=%f, Vel=%f, NumTent=%d, expF=%f>");
+	this->tentacles();
+	for(unsigned int t=0; t<numTent; t++){
+		this->tentacles.push_back(oryx_path_planner::Tentacle(expFact, seedRad, t, numTent, resolution, xDim, yDim, velocity));
+	}
+}
+
+oryx_path_planner::Tentacle& SpeedSet::getTentacle(int index){
+	return this->tentacles.at(index);
+}
 //***************************** TENTACLE GENERATOR *********************************//
 TentacleGenerator::TentacleGenerator(int numTentacles, double expFact, double resolution, double xDim, double yDim, std::vector<pair<double> >& speedSets){
 	this->expFact 		= expFact;
