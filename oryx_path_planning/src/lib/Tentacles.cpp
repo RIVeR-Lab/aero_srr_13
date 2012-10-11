20b31335be9917fc897498811a6fbaabf2958c1c
apanzica <apanzica@wpi.edu>
2012-10-11 17:04:55 -0400

    Updated member functions to reflect changes in Tentacles.h

--- oryx_path_planning/src/lib/Tentacles.cpp
+++ oryx_path_planning/src/lib/Tentacles.cpp
@@ -10,7 +10,7 @@
 namespace oryx_path_planner{
 
 TentacleGenerator::TentacleGenerator(int numTentacles, double expFact, std::vector<double>& speedSets):
-		tentacles(speedSets.size()){
+		speedSets(speedSets.size()){
 	this->expFact 		= expFact;
 	this->numTentacles	= numTentacles;
 	TentacleGenerator::generateTentacles(speedSets);
@@ -20,8 +20,8 @@ TentacleGenerator::~TentacleGenerator() {
 	// TODO Auto-generated destructor stub
 }
 
-void TentacleGenerator::getTentacle(int speedSet, int index, std::vector<pair<int> >& result){
-
+oryx_path_planner::Tentacle& TentacleGenerator::getTentacle(int speedSet, int index){
+	return this->speedSets.at(speedSet).getTentacle(index);
 }
 
 
@@ -29,13 +29,7 @@ void TentacleGenerator::getTentacle(int speedSet, int index, std::vector<pair<in
 void TentacleGenerator::generateTentacles(std::vector<double>& speedSets){
 	//Build the speed sets
 	for(unsigned int v=0; v<speedSets.size(); v++){
-		std::vector<std::vector<pair<int> > >& speedSet = this->tentacles.at(v);
-		speedSet.resize(numTentacles);
-		//Build the tentacles in a speed set
-		for(int t=0; t<speedSet.size(); t++){
-			std::vector<pair<int> >& tentacle = speedSet.at(t);
 
-		}
 	}
 }
 
