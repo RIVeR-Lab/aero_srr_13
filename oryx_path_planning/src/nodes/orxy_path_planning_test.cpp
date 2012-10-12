8ffe496233e96551ba0b6a8daf7adf362916899c
apanzica <apanzica@wpi.edu>
2012-10-11 20:03:20 -0400

    Fixed some bugs

--- oryx_path_planning/src/nodes/orxy_path_planning_test.cpp
+++ oryx_path_planning/src/nodes/orxy_path_planning_test.cpp
@@ -8,16 +8,21 @@
 #include "Tentacles.h"
 
 
-int int main(int argc, char **argv) {
+int main(int argc, char **argv) {
 	ros::init(argc, argv, "oryx_path_planning_test");
-	std::vector<oryx_path_planner::pair<double> > speedSets;
+	ros::NodeHandle nh;
+	std::vector<pair<double> > speedSets;
+	ROS_INFO("Testing Tentacle Generator...");
 	for(int i=0; i<16; i++){
-		oryx_path_planner::pair<double> speedData = new oryx_path_planner::pair<double>();
+		pair<double> speedData;
 		speedData.a = (i+1)*5;
 		speedData.b = 2/(16-i);
 		speedSets.push_back(speedData);
+		//delete &speedData;
 	}
-	oryx_path_planner::TentacleGenerator(82, 1.19, .25, 512*.25, 512*.25, speedSets);
+	TentacleGenerator(82, 1.19, .25, 512*.25, 512*.25, speedSets);
+
+	return 0;
 }
 
 
