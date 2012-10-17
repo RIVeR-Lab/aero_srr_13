66ec14fa84f66daee259e1ca7a5e8e1306e9dc15
apanzica <apanzica@wpi.edu>
2012-10-17 17:41:34 -0400

    Updated test to print out all of the speed sets

--- oryx_path_planning/src/nodes/orxy_path_planning_test.cpp
+++ oryx_path_planning/src/nodes/orxy_path_planning_test.cpp
@@ -7,24 +7,26 @@
 #include <ros/ros.h>
 #include "Tentacles.h"
 
-void printSpeedSet(int xDim, int yDim, oryx_path_planning::SpeedSet& speedSet);
+void printSpeedSet(int xDim, int yDim, double resoltuion, oryx_path_planning::SpeedSet& speedSet);
 
 int main(int argc, char **argv) {
 	ros::init(argc, argv, "oryx_path_planning_test");
 	ros::NodeHandle nh;
 
 	ROS_INFO("Testing Tentacle Generator...");
-	int xDim=50;
+	int xDim=25;
 	int yDim=xDim;
 	double resolution = .25;
-	int numTent= 11;
+	int numTent= 23;
 	double minSpeed = .1;
 	double maxSpeed = 1.5;
 	int numSpeedSet = 5;
 	double expFact = 1.15;
 	oryx_path_planning::TentacleGenerator generator(minSpeed, maxSpeed, numSpeedSet, numTent, expFact, resolution, xDim, yDim);
 	ROS_INFO("Tentacles Generated. Printing Speed Sets...");
-	printSpeedSet(xDim, yDim, generator.getSpeedSet(0));
+	for(int s=0; s<numSpeedSet; s++){
+		printSpeedSet(xDim, yDim, resolution, generator.getSpeedSet(s));
+	}
 
 	/*	tf::Point test1;
 	tf::Point test2;
@@ -42,22 +44,22 @@ int main(int argc, char **argv) {
 }
 
 
-void printSpeedSet(int xDim, int yDim, oryx_path_planning::SpeedSet& speedSet){
-	int xSize = xDim;
-	int ySize = yDim*2;
+void printSpeedSet(int xDim, int yDim, double resolution, oryx_path_planning::SpeedSet& speedSet){
+	int xSize = xDim/resolution;
+	int ySize = (yDim/resolution)*2;
 	int numTent = speedSet.getNumTentacle();
 	std::vector<std::string> occGrid(xSize, std::string(ySize, ' '));
 
 	for(unsigned int t=0; t<numTent; t++){
 		//if(t!=numTent/2){
-			oryx_path_planning::Tentacle tentacle = speedSet.getTentacle(t);
-			for(unsigned int p=0; p<tentacle.getPoints().size(); p++){
-				tf::Point point = tentacle.getPoints().at(p);
-				ROS_INFO("Got Point at <%f, %f>", point.getX(), point.getY());
-				point.setY(point.getY()+yDim);
-				ROS_INFO("Placing Point at <%f, %f>", point.getX(), point.getY());
-				occGrid.at((int)point.getX()).replace((int)point.getY(),1,"T");
-			}
+		oryx_path_planning::Tentacle tentacle = speedSet.getTentacle(t);
+		for(unsigned int p=0; p<tentacle.getPoints().size(); p++){
+			tf::Point point = tentacle.getPoints().at(p);
+			//ROS_INFO("Got Point at <%f, %f>", point.getX(), point.getY());
+			point.setY(point.getY()+ySize/2);
+			//ROS_INFO("Placing Point at <%f, %f>", point.getX(), point.getY());
+			occGrid.at(oryx_path_planning::roundToGrid(point.getX(), resolution)).replace(oryx_path_planning::roundToGrid(point.getY(), resolution),1,"T");
+		}
 		//}
 	}
 	std::string output;
