d7d6a5c271560cdde11d2db8f815bb720eeacbe1
apanzica <apanzica@wpi.edu>
2012-10-17 20:49:00 -0400

    Added a basic chained exception class which extends std::exception. Takes in a message and an optional cause

--- oryx_path_planning/src/OryxPathPlanning.h
+++ oryx_path_planning/src/OryxPathPlanning.h
@@ -12,6 +12,77 @@
 
 namespace oryx_path_planning{
 
+const double PI = std::atan(1.0)*4;	///Since C++ lacks a predefined PI constant, define it here
+
+/**
+ * @author Adam Panzics
+ * @brief basic chainable exception class
+ *
+ * Provides an implementation of std::exception which allows for chaining of exceptions
+ */
+class ChainableException: public std::exception{
+public:
+	/**
+	 * Standard Copy constructor
+	 * @param exception The exception to initialize this exception's fields to
+	 */
+	ChainableException(oryx_path_planning::ChainableException exception){
+		this->message(exception.message);
+		this->cause(exception.cause);
+	}
+
+	/**
+	 * Constructor for creating a new exception without a cause
+	 * @param message The error message for this exception
+	 */
+	ChainableException(std::string& message):
+	message(message),
+	cause("ROOT"){
+		this->message += "\nCaused By-> ";
+		this->message += std::string(cause.what());
+	}
+	/**
+	 * Constructor for creating a new exception with a cause
+	 * @param message	The error message for the exception
+	 * @param cause		The exception which caused this exception
+	 */
+	ChainableException(std::string& message, std::exception& cause):
+	message(message),
+	cause(cause){
+		this->message += "\nCaused By-> ";
+		this->message += std::string(cause.what());
+	}
+	/**
+	 * Deletes the message and cause fields
+	 */
+	virtual ~ChainableException(){
+		delete this->message;
+		delete this->cause;
+	}
+
+	/**
+	 * Returns the chained error message in the form of:
+	 *
+	 * "this_message \n Caused By-> that_message"
+	 * @return A char* to a null-terminated character array containing the chained error message
+	 */
+	virtual const char* what() const throw()
+	 {
+	    return this->message.c_str();
+	 }
+
+	/**
+	 * Gets the raw exception that caused this exception
+	 * @return A std::exception which caused this exception
+	 */
+	std::exception& getCause(){
+		return this->cause;
+	}
+protected:
+	std::string		message;	///The error message of this ChaingedException
+	std::exception	cause;		///The exception which caused this ChaingedException
+};
+
 /**
  * Rounds a value to the nearest scale point. EX: raw=12.35, frac=.25, return = 12.25
  * @param raw Raw value to round
