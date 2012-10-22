9abdd2a11c075c2df34d2d10ac0d499d75faabbf
apanzica <apanzica@wpi.edu>
2012-10-22 14:07:46 -0400

    The new header file which includes everything that makes up the oryx_path_planning namespace

---
+++ oryx_path_planning/src/OryxPathPlanning.h
@@ -0,0 +1,16 @@
+/**
+ * @file	OryxPathPlanning.h
+ * @date	Oct 22, 2012
+ * @author	Adam Panzica
+ * @brief	Contains all of the Includes required to complete the oryx_path_planning namespace
+ */
+
+#ifndef ORYXPATHPLANNING_H_
+#define ORYXPATHPLANNING_H_
+
+#include"OryxPathPlanningUtilities.h"	///General Utility definitions
+#include"Tentacles.h"					///Tentacle class definitions
+
+
+
+#endif /* ORYXPATHPLANNING_H_ */
