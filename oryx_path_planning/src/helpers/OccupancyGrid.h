21243cb017c962e0ca82b59d948ee5275ea96a2f
apanzica <apanzica@wpi.edu>
2012-10-22 16:41:48 -0400

    Added the OccupancyGrid.h header, which defines a set of data structures which are used to define the occupancy grids that are used by the Tentacle based path planner.

---
+++ oryx_path_planning/src/helpers/OccupancyGrid.h
@@ -0,0 +1,181 @@
+/**
+ * @file	OccupancyGrid.h
+ * @date	Oct 22, 2012
+ * @author	Adam Panzica
+ * @brief	//TODO fill in detailed discription here
+ */
+
+#ifndef OCCUPANCYGRID_H_
+#define OCCUPANCYGRID_H_
+
+//*********************** SYSTEM DEPENDENCIES ************************************//
+#include <ros/ros.h>
+#include <sensor_msgs/PointCloud2.h>
+#include <pcl/ros/conversions.h>
+#include <pcl/point_cloud.h>
+#include <pcl/point_types.h>
+
+//*********************** LOCAL DEPENDENCIES ************************************//
+#include "OryxPathPlanningUtilities.h"
+
+
+//*********************** CLASS DEFINITIONS ************************************//
+namespace oryx_path_planning{
+
+/**
+ * @author Adam Panzica
+ * @brief Enum for describing the state of a point
+ */
+enum PointTrait{
+	OBSTACLE,        //!< OBSTACLE			Point on the grid contains an obstacle
+	INFLATED,		 //!< INFLATED			Point on the grid is occupied by a safety inflation radius
+	UNKNOWN,         //!< UNKNOWN			Point on the grid is unknown
+	FREE_HIGH_COST,	 //!< FREE_HIGH_COST	Point on the grid is free but is expensive to travel over
+	FREE_LOW_COST,   //!< FREE_LOW_COST		Point on the grid is free but is easy to travel over
+	GOAL             //!< GOAL				Point on the grid is the goal position
+};
+
+/**
+ * @author Adam Panzica
+ * @brief Struct which describes the data in a point in the occupancy grid
+ */
+struct PointXYZWithTrait: pcl::PointXYZ{
+public:
+	/**
+	 * @author Adam Panzica
+	 * @brief Default constructor which creates an empty point with its coords set to <0,0,0> and its PointTrait set to UNKNOWN
+	 */
+	inline PointXYZWithTrait():
+		pcl::PointXYZ(){
+		this->point_trait = UNKNOWN;
+	}
+
+	/**
+	 * @author	Adam Panzica
+	 * @brief	Constructor for creating a point with a set of coord, <x,y,z>, and a given PointTrait
+	 * @param x		The x coordinate of the point
+	 * @param y		The y coordinate of the point
+	 * @param z		The z coordinate of the point
+	 * @param trait	The PointTrait of the point
+	 */
+	inline PointXYZWithTrait(double x, double y, double z, oryx_path_planning::PointTrait trait):
+		pcl::PointXYZ(x, y, z){
+		this->point_trait = trait;
+	}
+
+	inline PointXYZWithTrait(oryx_path_planning::PointXYZWithTrait& point):
+		PointXYZ(point){
+		this->point_trait = point.getTrait();
+	}
+
+	/**
+	 * Default destructor
+	 */
+	inline virtual ~PointXYZWithTrait(){};
+
+	/**
+	 * @author	Adam Panzica
+	 * @brief	Gets the PointTrait of this point
+	 * @return	The PointTrait associated with this point
+	 */
+	inline oryx_path_planning::PointTrait getTrait(){
+		return this->point_trait;
+	}
+
+	/**
+	 * @author	Adam Panzica
+	 * @brief	Sets the PointTrait associated with this point
+	 * @param trait The PointTrait to set this point to
+	 */
+	inline void setTrait(oryx_path_planning::PointTrait trait){
+		this->point_trait = trait;
+	}
+protected:
+	oryx_path_planning::PointTrait point_trait;	///The PointTrait associated with the point
+};
+
+/**
+ * @author	Adam Panzica
+ * @brief	Class which represents an Occupancy Grid that contains data on the robot's local frame
+ */
+class OccupancyGrid{
+public:
+	/**
+	 * @author	Adam Panzica
+	 * @brief	Default constructor
+	 */
+	OccupancyGrid();
+	/**
+	 * @author	Adam Panzica
+	 * @brief	Default copy constructor
+	 * @param grid The OccupancyGrid to copy
+	 */
+	OccupancyGrid(oryx_path_planning::OccupancyGrid& grid);
+	/**
+	 * @author	Adam Panzica
+	 * @brief	Creates a new Occupancy Grid with a given set of dimensions and grid resolution
+	 * @param xDim			The size of the occupancy grid in real units in the x-axis
+	 * @param yDim			The size of the occupancy grid in real units in the y-axis
+	 * @param zDim			The size of the occupancy grid in real units in the y-axis
+	 * @param resolution	The grid resolution of the occupancy grid
+	 * @param seedTrait		The PointTrait to initialize the values in the occupancy grid to
+	 */
+	OccupancyGrid(double xDim, double yDim, double zDim, double resolution, oryx_path_planning::PointTrait seedTrait);
+	/**
+	 * Default destructor
+	 */
+	virtual ~OccupancyGrid();
+
+	/**
+	 * @author	Adam Panzica
+	 * @brief	Gets the PointTrait of a given point on the grid
+	 * @param x	The x-coord of the point
+	 * @param y	The y-coord of the point
+	 * @param z The z-coord of the point
+	 * @return The PointTrait of the point at the given coordinates
+	 */
+	oryx_path_planning::PointTrait getPointTrait(double x, double y, double z);
+
+	/**
+	 * @author	Adam Panzica
+	 * @brief	Sets the PointTrait of a point on the grid
+	 * @param x	The x-coord of the point
+	 * @param y	The y-coord of the point
+	 * @param z	The z-coord of the point
+	 * @param trait	The PointTrait to set the point to
+	 * @return True if succesful, else false
+	 */
+	bool setPointTrait(double x, double y, double z, oryx_path_planning::PointTrait trait);
+
+	/**
+	 * @author	Adam Panzica
+	 * @brief	Gets the whole PointCloud which backs this occupancy grid
+	 * @return	A boost::shared_pt containing the PointCloud<oryx_path_planning::PointXYZWithTrait> which backs this occupancy grid
+	 */
+	boost::shared_ptr<pcl::PointCloud<oryx_path_planning::PointXYZWithTrait>> getGrid();
+
+	/**
+	 * @author	Adam Panzica
+	 * @brief	Generates a sensor_msgs::PointCloud2 message from the data in the grid
+	 * @param message	The sensor_msgs::PointCloud2 to write the data to
+	 * @return	True if successful, else false
+	 */
+	bool generateMessage(sensor_msgs::PointCloud2& message);
+
+	/**
+	 * @author	Adam Panzic
+	 * @brief	Generates a string representation of a slice of the Occupancy Grid
+	 * @param sliceAxis	The axis to create the slice along. O=x-axis, 1 = y-axis, 2=z-axis
+	 * @param slice		The distance along the slice axis to make the slice
+	 * @return	A shared pointer to a std::string containing an ASCII-art representation of the occupancy grid slice specified
+	 */
+	boost::shared_ptr<std::string> toString(int sliceAxis, double slice);
+private:
+	double xDim;	///The x size of this grid
+	double yDim;	///The y size of this grid
+	double zDim;	///The z size of this grid
+	double res;		///The grid resolution of this occupancy grid
+	boost::shared_ptr<pcl::PointCloud<oryx_path_planning::PointXYZWithTrait> > occGrid;		///A smart pointer to the point cloud which contains the data for this occupancy grid
+};
+};
+#endif /* OCCUPANCYGRID_H_ */
