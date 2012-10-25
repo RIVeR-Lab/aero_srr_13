ad16e07a5370052bb13fc5b2b56ef26cb8fbd085
root <root@parallels-Parallels-Virtual-Platform>
2012-10-25 17:34:26 -0400

    Added a basic node for sending test occupancy grid messages

---
+++ oryx_path_planning/src/nodes/occupancy_generator.cpp
@@ -0,0 +1,81 @@
+/**
+ * @file	occupancy_generator.cpp
+ * @date	Oct 25, 2012
+ * @author	Adam Panzica
+ * @brief	simple node for generating test occupancy grids
+ */
+#include<ros/ros.h>
+#include<sensor_msgs/PointCloud2.h>
+#include"OccupancyGrid.h"
+
+///Macro for printing out warning messages if default parameters are used
+#define PARAM_WARN(param,value) ROS_WARN(warn_message.c_str(), param.c_str(), value.c_str())
+
+int main(int argc, char **argv) {
+	std::string warn_message("Parameter <%s> Not Set. Using Default Value <%s>");
+	ros::init(argc, argv, "Occupancy_Generator");
+	ros::NodeHandle nh;
+	ros::Publisher pub = nh.advertise<sensor_msgs::PointCloud2>("occupancy_point_cloud_topic", 2);
+
+	//x dimension of occupancy grid
+	std::string p_x_dim("occupancy/x_dimension");
+	double xDim = 20;
+	std::string xDim_msg("");
+	xDim_msg+= boost::lexical_cast<double>(xDim);
+	xDim_msg+="m";
+
+	//y dimension of occupancy grid
+	std::string p_y_dim("occupancy/y_dimension");
+	double yDim = 20;
+	std::string yDim_msg("");
+	yDim_msg+= boost::lexical_cast<double>(yDim);
+	yDim_msg+="m";
+
+	//z dimension of occupancy grid
+	std::string p_z_dim("occupancy/z_dimension");
+	double zDim = 0;
+	std::string zDim_msg("");
+	zDim_msg+= boost::lexical_cast<double>(zDim);
+	zDim_msg+="m";
+
+	//resolution occupancy grid
+	std::string p_res("occupancy/grid_resolution");
+	double res = .25;
+	std::string p_res_msg("");
+	p_res_msg+= boost::lexical_cast<double>(res);
+	p_res_msg+="m";
+
+	if(!nh.getParam(p_x_dim,	xDim))			PARAM_WARN(p_x_dim,		xDim_msg);
+	if(!nh.getParam(p_y_dim,	yDim))			PARAM_WARN(p_y_dim,		yDim_msg);
+	if(!nh.getParam(p_z_dim,	zDim))			PARAM_WARN(p_z_dim,		zDim_msg);
+	if(!nh.getParam(p_res,		res))			PARAM_WARN(p_res,		p_res_msg);
+
+
+	while(ros::ok()){
+		double input;
+		ROS_INFO("Enter A Number: ");
+		std::cin >> input;
+		ROS_INFO("I Got Input!: %f", input);
+		boost::shared_ptr<oryx_path_planning::OccupancyGrid> grid_ptr(new oryx_path_planning::OccupancyGrid(xDim, yDim, res, oryx_path_planning::UNKNOWN));
+		double y;
+		for(double x=0; x<xDim; x+=res){
+			y= input*x;
+			if(y>yDim){
+				y=yDim;
+			}else if(y< 0){
+				y=0;
+			}
+			try{
+				ROS_INFO("Setting Point at <%f,%f>", x,y);
+				grid_ptr->setPointTrait(x,y,0,oryx_path_planning::FREE_HIGH_COST);
+			}catch(std::exception& e){
+				ROS_ERROR(e.what());
+			}
+		}
+		sensor_msgs::PointCloud2 message;
+		grid_ptr->generateMessage(message);
+		pub.publish(message);
+	}
+}
+
+
