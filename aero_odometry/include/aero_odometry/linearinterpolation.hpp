1e5fcf2e49d69ec781f57047204eda6dcc09196a
apanzica <apanzica@wpi.edu>
2013-01-30 08:11:26 -0500

    Changed from using tf::Transform to geometry_msgs::Pose/PoseStamped to simplify performing interpolation

--- aero_odometry/include/aero_odometry/linearinterpolation.hpp
+++ aero_odometry/include/aero_odometry/linearinterpolation.hpp
@@ -15,13 +15,17 @@
 //******************* SYSTEM DEPENDANCIES ****************//
 #include<ros/ros.h>
 #include<tf/tf.h>
+#include<geometry_msgs/Pose.h>
 //******************* LOCAL DEPENDANCIES ****************//
 
 //*********************** NAMESPACES ********************//
+using geometry_msgs::Pose;
+using geometry_msgs::PoseStamped;
+using ros::Time;
 namespace aero_odometry
 {
 /**
- * @brief Linearly interpolates between two transform matrices based on time
+ * @brief Linearly interpolates between two poses based on time
  * @param previous
  * @param previous_time
  * @param current
@@ -29,16 +33,16 @@ namespace aero_odometry
  * @param interp_time
  * @param result
  */
-void linearInterpolate(const tf::Transform& previous, const ros::Time& previous_time, const tf::Transform& current, const ros::Time& current_time, const ros::Time& interp_time, tf::Transform& result);
+void linearInterpolate(const Pose& previous, const Time& previous_time, const Pose& current, const Time& current_time, const Time& interp_time, Pose& result);
 
 /**
- * @brief Linearly interpolates between two transform matrices based on time
+ * @brief Linearly interpolates between two poses based on time
  * @param previous
  * @param current
  * @param interp_time
  * @param result
  */
-void linearInterpolate(const tf::StampedTransform& previous, const tf::StampedTransform& current, const ros::Time& interp_time, tf::StampedTransform& result);
+void linearInterpolate(const PoseStamped& previous, const PoseStamped& current, const Time& interp_time, PoseStamped& result);
 }; /* END aero_orometry */
 
 #endif /* LINEARINTERPOLATION_HPP_ */
