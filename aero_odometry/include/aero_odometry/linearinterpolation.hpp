7459410a908a7b7199e23a3c9ae80e23a78ac0cd
apanzica <apanzica@wpi.edu>
2013-01-30 10:42:52 -0500

    Implemented the linear interpolation functions using standard Quaternion/Point interpolation to go between poses.

--- aero_odometry/include/aero_odometry/linearinterpolation.hpp
+++ aero_odometry/include/aero_odometry/linearinterpolation.hpp
@@ -22,25 +22,43 @@
 using geometry_msgs::Pose;
 using geometry_msgs::PoseStamped;
 using ros::Time;
+using tf::Quaternion;
+using tf::Point;
+
 namespace aero_odometry
 {
 /**
+ * @author Adam Panzica
  * @brief Linearly interpolates between two poses based on time
- * @param previous
- * @param previous_time
- * @param current
- * @param current_time
- * @param interp_time
- * @param result
+ * @param [in]  previous       The pose at the last measurement time
+ * @param [in]  previous_time  The ros::Time of the last measurement
+ * @param [in]  current        The pose at the current measurement time
+ * @param [in]  current_time   The ros::Time of the current measurement
+ * @param [in]  interp_time    The ros::Time that should be interpolated to
+ * @param [out] result         The resultant interpolated pose
+ *
+ * Uses standard linear interpolation of the Pose Quaternion and Position to determine the resultant Pose.
+ *
+ * Note that this function assumes that each pose is relative to the same frame coordinate system
  */
 void linearInterpolate(const Pose& previous, const Time& previous_time, const Pose& current, const Time& current_time, const Time& interp_time, Pose& result);
 
 /**
+ * @author Adam Panzica
  * @brief Linearly interpolates between two poses based on time
- * @param previous
- * @param current
- * @param interp_time
- * @param result
+ * @param [in]  previous       The pose and time of the last measurement
+ * @param [in]  current        The pose and time of the current measurement
+ * @param [in]  interp_time    The ros::Time to interpolate to
+ * @param [out] result         The resultant interpolated pose
+ *
+ * Uses standard linear interpolation of the Pose Quaternion and Position to determine the resultant Pose.
+ *
+ * Note that the current and previous measurement times are extracted from the geometry_msgs::PoseStamped Header, so those fields
+ * must be full for the function to work correctly. The header for the resultant geometry_msgs::PoseStamped is not filled in
+ * by this method. Currently, assumes both poses are in the same frame.
+ *
+ * \todo extend to use tf to allow for poses in different frames to be interpolated to, and for an arbitrary output frame to be
+ * specified
  */
 void linearInterpolate(const PoseStamped& previous, const PoseStamped& current, const Time& interp_time, PoseStamped& result);
 }; /* END aero_orometry */
--- aero_odometry/src/helpers/linearinterpoloation.cpp
+++ aero_odometry/src/helpers/linearinterpoloation.cpp
@@ -12,16 +12,34 @@
 //******************* LOCAL DEPENDANCIES ****************//
 #include<aero_odometry/linearinterpolation.hpp>
 //*********************** NAMESPACES ********************//
-using namespace aero_odometry;
 
 
-void aero_odometry::linearInterpolate(const tf::Transform& previous, const ros::Time& previous_time, const tf::Transform& current, const ros::Time& current_time, const ros::Time& interp_time, tf::Transform& result)
+namespace aero_odometry
 {
+void aero_odometry::linearInterpolate(const Pose& previous, const Time& previous_time, const Pose& current, const Time& current_time, const Time& interp_time, Pose& result)
+{
+	//extract the position/rotation data
+	Quaternion pq(previous.orientation);
+	Quaternion cq(current.orientation);
+	Point      pp(previous.orientation);
+	Point      cp(current.position);
+	//Calculate the %change in time
+	double scale        = (interp_time-previous_time)/(current_time-previous_time);
+
+	//Linearly interpolate the orientation Quaternion
+	tf::quaternionTFToMsg((((cq - pq)*scale+pq).normalize()), result.orientation);
+
+	//Linearly interpolate the position point
+	tf::pointTFToMsg(((cp - pp)*scale+pp), result.position);
 
 }
 
-void aero_odometry::linearInterpolate(const tf::StampedTransform& previous, const tf::StampedTransform& current, const ros::Time& interp_time, tf::StampedTransform& result)
+void aero_odometry::linearInterpolate(const PoseStamped& previous, const PoseStamped& current, const Time& interp_time, PoseStamped& result)
 {
-
+	//Extract times
+	Time p_time(previous.header.stamp.sec, previous.header.stamp.nsec);
+	Time c_time(current.header.stamp.sec,  current.header.stamp.nsec);
+	aero_odometry::linearInterpolate(previous.pose, p_time, current.pose, c_time, interp_time, result.pose);
 }
+}; /*END NAMESPACE aero_odometry */
 
