af4dcbf922c486adb5e452bf561e11149d86742d
aruisdante <adampanzica@gmail.com>
2013-02-19 19:15:46 -0500

    Added GlobalPlanner class
    Added the Global Planner class, which will define the basic
    functionality of the aero_global_planner node. Currently just a basic
    shell implementation to get the fundemental ideas down.
    
    #16 #17

---
+++ aero_path_planning/src/nodes/global_planner.cpp
@@ -0,0 +1,50 @@
+/**
+ * @file global_planner.cpp
+ *
+ * @date   Feb 19, 2013
+ * @author Adam Panzica
+ * @brief \todo
+ */
+
+//License File
+
+//****************SYSTEM DEPENDANCIES**************************//
+
+//*****************LOCAL DEPENDANCIES**************************//
+#include<aero_path_planning/GlobalPlanner.h>
+//**********************NAMESPACES*****************************//
+
+
+using namespace aero_path_planning;
+
+GlobalPlanner::GlobalPlanner(ros::NodeHandle& nh, ros::NodeHandle& p_nh, aero_path_planning::CarrotPathFinder& path_planner):
+		state_(MANUAL),
+		path_planner_(path_planner),
+		nh_(nh),
+		p_nh_(p_nh)
+{
+	ROS_INFO("Initializing Global Planner...");
+
+	ROS_INFO("Loading Occupancy Grid Parameters...");
+
+	ROS_INFO("Registering Topics...");
+
+
+	ROS_INFO("Aero Global Planner Running!");
+
+}
+
+GlobalPlanner::~GlobalPlanner()
+{
+
+}
+
+void GlobalPlanner::loadOccupancyParam()
+{
+
+}
+
+void GlobalPlanner::registerTopics()
+{
+
+}
