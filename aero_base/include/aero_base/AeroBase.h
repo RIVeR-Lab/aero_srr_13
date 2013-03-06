b98c43e119907ee5ae7c8cc3b2437407b53f61ff
Adam Panzica <adampanzica@gmail.com>
2013-03-06 11:18:50 -0500

    Modified manifest to depend on TF instead of TF2 as TF2 isn't prime time yet.

---
+++ aero_base/include/aero_base/AeroBase.h
@@ -0,0 +1,21 @@
+/**
+ * @file   AeroBase.h
+ *
+ * @date   Mar 6, 2013
+ * @author parallels
+ * @brief  \TODO
+ */
+
+#ifndef AEROBASE_H_
+#define AEROBASE_H_
+
+//*********** SYSTEM DEPENDANCIES ****************//
+#include <ros/ros.h>
+#include <tf/tf.h>
+//************ LOCAL DEPENDANCIES ****************//
+
+//***********    NAMESPACES     ****************//
+
+
+
+#endif /* AEROBASE_H_ */
