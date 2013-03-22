32997e5b708922ff5964a2096c5e8b5ab596d6ca
Adam Panzica <adampanzica@gmail.com>
2013-03-22 11:43:45 -0400

    Added basic functionlaity for switching between manual mode and autonomous mode #36

--- aero_srr_supervisor/include/aero_srr_supervisor/Supervisor.h
+++ aero_srr_supervisor/include/aero_srr_supervisor/Supervisor.h
@@ -28,6 +28,24 @@ private:
 
 	/**
 	 * @author Adam Panzica
+	 * @brief  Enum over the states that Supervisor can be in
+	 */
+	enum State
+	{
+		ERROR,    //!< ERROR The robot has encountered an error
+		STARTUP,  //!< STARTUP The robot is currently in its startup sequence
+		MANUAL,   //!< MANUAL The robot is currently in manual control mode
+		SEARCHING,//!< SEARCHING The robot is currently exploring the map
+		NAVOBJ,   //!< NAVOBJ The robot is currently navigating to an object
+		COLLECT,  //!< COLLECT The robot is currently collecting an object of interest
+		HOME,     //!< HOME The robot is currently returning to the home platform
+		PAUSE,    //!< PAUSE The robot is currently paused
+		SHUTDOWN, //!< SHUTDOWN The robot is currently in its shutdown sequence
+		SAFESTOP  //!< SAFESTOP The robot is currently in a safe-stop configuration
+	};
+
+	/**
+	 * @author Adam Panzica
 	 * @brief  Initialization function, Loads parameters from the ROS param server
 	 */
 	void loadParams();
@@ -49,11 +67,25 @@ private:
 	 */
 	void setCtrlMdCB(const aero_srr_supervisor::SetControlModeConstPtr& message);
 
+	/**
+	 * @author Adam Panzica
+	 * @brief  Callback for handling state updates
+	 * @param event
+	 */
+	void stateUptdCb(const ros::TimerEvent& event);
+
+	State state_;
+
 	std::string ctrlmd_topic_;
+	std::string aero_state_topic_;
 
 	ros::NodeHandle nh_;
 	ros::NodeHandle p_nh_;
+
 	ros::Subscriber ctrlmd_sub_;
+	ros::Publisher  aero_state_pub_;
+
+	ros::Timer      state_update_timer_;
 
 	tf::TransformListener transformer_;
 };
