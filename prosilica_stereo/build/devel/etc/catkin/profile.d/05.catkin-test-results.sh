c171932c637351440536c222ea6a63e4772d1370
bpwiselybabu <bpwiselybabu@bpwiselybabu-Studio-XPS-1340.(none)>
2013-03-24 12:55:14 -0400

    added gitignore

--- prosilica_stereo/build/devel/etc/catkin/profile.d/05.catkin-test-results.sh
+++
@@ -1,4 +0,0 @@
-# generated from catkin/cmake/env-hooks/05.catkin-test-results.sh.develspace.em
-
-export CATKIN_TEST_RESULTS_DIR="/home/bpwiselybabu/groovy_workspace/aero_srr/prosilica_stereo/build/test_results"
-export ROS_TEST_RESULTS_DIR=$CATKIN_TEST_RESULTS_DIR
--- prosilica_stereo/build/devel/setup.bash
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-# generated from catkin/cmake/templates/setup.bash.in
-
-CATKIN_SHELL=bash
-. "/home/bpwiselybabu/groovy_workspace/aero_srr/prosilica_stereo/build/devel/setup.sh"
