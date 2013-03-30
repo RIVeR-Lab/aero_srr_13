ea2c12dfd6d56daf7aa3018611470e2811ead7cb
Adam Panzica <adampanzica@gmail.com>
2013-03-29 22:38:05 -0400

    Begining of implementation of A* CarrotPlanner #45
    Created node structure for use with A*

---
+++ aero_path_planning/include/aero_path_planning/AStarCarrot.h
@@ -0,0 +1,106 @@
+/**
+ * @file   AStarCarrot.h
+ *
+ * @date   Mar 29, 2013
+ * @author Adam Panzica
+ * @brief  Class definitions of the A* algorithm as a CarrotPathFinder
+ */
+
+#ifndef ASTARCARROT_H_
+#define ASTARCARROT_H_
+
+//*********** SYSTEM DEPENDANCIES ****************//
+#include <queue>
+#include <ros/ros.h>
+#include <boost/shared_ptr.hpp>
+//************ LOCAL DEPENDANCIES ****************//
+#include <aero_path_planning/CarrotPathFinder.h>
+//***********    NAMESPACES     ****************//
+
+namespace aero_path_planning
+{
+
+namespace astar_utilities
+{
+typedef boost::function<double (aero_path_planning::Point, aero_path_planning::Point)> huristic_func;
+typedef boost::function<double (aero_path_planning::Point, aero_path_planning::Point)> cost_func;
+
+/**
+ * @author Adam Panzica
+ * @brief  Calculates a point huristic based on euclidian distance
+ * @param [in] point The point to measure the huristic from
+ * @param [in] goal  The goal point to measure the huristic to
+ * @return The euclidian distance between the points
+ */
+double eculidian_distance_huristic(const aero_path_planning::Point& point, const aero_path_planning::Point& goal);
+
+/**
+ * @author Adam Panzica
+ * @brief  Calculates the PointTrait weighted cost of moving through a point
+ * @param [in] this_point The point to calculate the cost for
+ * @param [in] last_point The last point in the chain to use for calculating cost
+ * @return The cost for moving to this_point @todo fill in actual math here
+ */
+double point_trait_cost(const aero_path_planning::Point& this_point, const aero_path_planning::Point& last_point);
+
+class AStarNode
+{
+public:
+	typedef boost::shared_ptr<AStarNode> AStarNodePtr;
+	AStarNode();
+	AStarNode(const AStarNodePtr& copy);
+	AStarNode(const AStarNode&    copy);
+	AStarNode(const Point& location, const Point& goal, const AStarNodePtr parent, cost_func cost, huristic_func huristic);
+
+	/**
+	 * @author Adam Panzica
+	 * @return The location of this node
+	 */
+	const Point& getLocation();
+
+	/**
+	 * @author Adam Panzica
+	 * @return The cost of moving through this node
+	 */
+	double getG() const;
+
+	/**
+	 * @author Adam Panzica
+	 * @return The huristic value of this node
+	 */
+	double getH() const;
+
+	/**
+	 * @author Adam Panzica
+	 * @return The fitness of this node, = H+G
+	 */
+	double getF() const;
+
+	AStarNode& operator= (AStarNode const & rhs);
+	bool       operator< (AStarNode const & rhs) const;
+	bool       operator<=(AStarNode const & rhs) const;
+	bool       operator> (AStarNode const & rhs) const;
+	bool       operator>=(AStarNode const & rhs) const;
+	bool       operator==(AStarNode const & rhs) const;
+
+private:
+	aero_path_planning::Point location_;
+	AStarNodePtr parent_;
+	double g_;
+	double h_;
+	double f_;
+};
+
+}
+
+class AStarCarrot : public aero_path_planning::CarrotPathFinder
+{
+public:
+	AStarCarrot();
+};
+
+}
+
+
+
+#endif /* ASTARCARROT_H_ */
---
+++ aero_path_planning/src/helpers/AStarCarrot.cpp
@@ -0,0 +1,131 @@
+/**
+ * @file   AStartCarrot.cpp
+ *
+ * @date   Mar 29, 2013
+ * @author Adam Panzica
+ * @brief  Implementation of the A* algorithm as a CarrotPathFinder
+ */
+
+//*********** SYSTEM DEPENDANCIES ****************//
+#include <pcl/registration/distances.h>
+//************ LOCAL DEPENDANCIES ****************//
+#include <aero_path_planning/AStarCarrot.h>
+//***********    NAMESPACES     ****************//
+
+using namespace aero_path_planning;
+using namespace aero_path_planning::astar_utilities;
+
+double ed_huristic(const aero_path_planning::Point& point, const aero_path_planning::Point& goal)
+{
+	return pcl::distances::l2(point.getVector4fMap(), goal.getVector4fMap());
+}
+
+double pt_cost(const aero_path_planning::Point& this_point, const aero_path_planning::Point& last_point)
+{
+	double pcost = 0;
+
+	switch(this_point.rgba)
+	{
+	case aero_path_planning::UNKNOWN:
+	case aero_path_planning::FREE_LOW_COST:
+	case aero_path_planning::GOAL:
+		pcost = 1.0;
+		break;
+	case aero_path_planning::FREE_HIGH_COST:
+		pcost = 2.0;
+		break;
+	case aero_path_planning::TRAVERSED:
+		pcost = 1.25;
+		break;
+	default:
+		pcost = 1;
+		break;
+	}
+
+	return ed_huristic(this_point, last_point)*pcost;
+}
+
+AStarNode::AStarNode():
+		g_(0),
+		h_(0),
+		f_(std::numeric_limits<double>::infinity())
+{
+
+}
+
+AStarNode::AStarNode(const AStarNode& copy)
+{
+	*this = copy;
+}
+
+AStarNode::AStarNode(const AStarNodePtr& copy)
+{
+	*this = *copy;
+}
+
+AStarNode::AStarNode(const Point& location, const Point& goal, const AStarNodePtr parent, cost_func cost, huristic_func huristic):
+		location_(location),
+		parent_(parent),
+		h_(huristic(location, goal))
+{
+	if(parent != AStarNodePtr())
+	{
+		this->g_ = cost(location, parent->getLocation())+parent->getG();
+	}
+	else
+	{
+		this->g_ = 0;
+	}
+	this->f_ = this->h_+this->g_;
+}
+
+double AStarNode::getG() const
+{
+	return this->g_;
+}
+
+double AStarNode::getH() const
+{
+	return this->h_;
+}
+
+double AStarNode::getF() const
+{
+	return this->f_;
+}
+
+AStarNode& AStarNode::operator= (AStarNode const & rhs)
+{
+	this->location_ = rhs.location_;
+	this->parent_   = rhs.parent_;
+	this->f_        = rhs.f_;
+	this->g_        = rhs.g_;
+	this->h_        = rhs.h_;
+	return *this;
+}
+
+bool       AStarNode::operator< (AStarNode const & rhs) const
+{
+	return this->getF() < rhs.getF();
+}
+bool       AStarNode::operator> (AStarNode const & rhs) const
+{
+	return this->getF() > rhs.getF();
+}
+bool       AStarNode::operator<= (AStarNode const & rhs) const
+{
+	return this->getF() <= rhs.getF();
+}
+bool       AStarNode::operator>= (AStarNode const & rhs) const
+{
+	return this->getF() >= rhs.getF();
+}
+bool       AStarNode::operator==(AStarNode const & rhs) const
+{
+	return this->getF() == rhs.getF();
+}
+
+
+
+
+
