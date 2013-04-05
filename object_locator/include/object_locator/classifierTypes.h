e81f08b4d77975eda9e4289462a107c47465ef7c
szutshi <szutshi@wpi.edu>
2013-04-05 16:42:14 -0400

    Added classification object types header for detection manager.

---
+++ object_locator/include/object_locator/classifierTypes.h
@@ -0,0 +1,22 @@
+/*
+ * classifierTypes.h
+ *
+ *  Created on: Apr 5, 2013
+ *      Author: srr
+ */
+
+#ifndef CLASSIFIERTYPES_H_
+#define CLASSIFIERTYPES_H_
+
+namespace object_locator
+{
+	enum object_type
+	{
+		WHA,
+		PINK_BALL
+	};
+}
+
+
+
+#endif /* CLASSIFIERTYPES_H_ */
