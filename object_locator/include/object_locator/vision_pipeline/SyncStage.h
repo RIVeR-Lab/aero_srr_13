628e0cce39dbd60f4bbdd9ac32c9fec0148039ab
szutshi <szutshi@wpi.edu>
2013-04-22 22:00:02 -0400

    Changed Stages to use ros disparity.

--- object_locator/include/object_locator/vision_pipeline/SyncStage.h
+++ object_locator/include/object_locator/vision_pipeline/SyncStage.h
@@ -27,19 +27,23 @@ namespace object_locator
 		void registerTopics();
 		virtual void leftImageCb(const sensor_msgs::ImageConstPtr& msg, const sensor_msgs::CameraInfoConstPtr& cam_info);
 		virtual void rightImageCb(const sensor_msgs::ImageConstPtr& msg, const sensor_msgs::CameraInfoConstPtr& cam_info);
-		virtual void generateSyncMsg(object_locator::SyncImageMsg& msg);
+		virtual void disparityImageCb(const sensor_msgs::ImageConstPtr& msg);
+		virtual void generateSyncMsg(object_locator::SyncImagesAndDisparity& msg);
 		virtual void gotImages();
-		std::string left_input_topic_, right_input_topic_,output_topic_;
+		std::string left_input_topic_, right_input_topic_,output_topic_, disparity_input_topic_;
 		image_transport::ImageTransport* it_;
 		image_transport::CameraSubscriber image_left_;
 		image_transport::CameraSubscriber image_right_;
+		image_transport::Subscriber disparity_;
 		ros::Publisher sync_image_pub_;
 		sensor_msgs::Image left_image_;
 		sensor_msgs::Image right_image_;
+		sensor_msgs::Image disparity_image_;
 		sensor_msgs::CameraInfo left_info_;
 		sensor_msgs::CameraInfo right_info_;
-		bool gotLeft;
-		bool gotRight;
+		bool gotLeft_;
+		bool gotRight_;
+		bool gotDisparity_;
 	};
 
 }
