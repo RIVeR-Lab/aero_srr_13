30fbe0dba3b5c77768ee550897d5a84ec69a2c51
szutshi <szutshi@wpi.edu>
2013-04-29 19:46:34 -0400

    SyncStage pub wrong type of msg.

--- object_locator/src/helpers/vision_pipeline/SyncStage.cpp
+++ object_locator/src/helpers/vision_pipeline/SyncStage.cpp
@@ -41,7 +41,7 @@ void SyncStage::registerTopics()
 	this->image_left_ = it_->subscribeCamera(this->left_input_topic_,2,&SyncStage::leftImageCb,this);
 	this->image_right_ = it_->subscribeCamera(this->right_input_topic_,2,&SyncStage::rightImageCb,this);
 //	this->disparity_ = this->getNodeHandle().subscribe(this->disparity_input_topic_,2,&SyncStage::disparityImageCb,this);
-	this->sync_image_pub_ = this->getNodeHandle().advertise<object_locator::SyncImagesAndDisparity>(this->output_topic_,2);
+	this->sync_image_pub_ = this->getNodeHandle().advertise<object_locator::SyncImageMsg>(this->output_topic_,2);
 }
 
 void SyncStage::imageCb(const object_locator::SyncImageMsgConstPtr& msg)
