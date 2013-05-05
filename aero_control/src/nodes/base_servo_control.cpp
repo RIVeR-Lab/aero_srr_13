c93469623dff83d083fbe4424ee2ed7ff54f681a
mpdedonato <mdedonato@ubuntu.ubuntu-domain>
2013-05-05 18:04:05 -0400

    cleaned up folders

--- aero_control/src/nodes/base_servo_control.cpp
+++ aero_control/src/nodes/base_servo_control.cpp
@@ -19,13 +19,14 @@
 
 using namespace aero_control;
 
-BaseServoController::BaseServoController(ros::NodeHandle nh, std::string DesiredPosition,
+BaseServoController::BaseServoController(ros::NodeHandle nh, std::string DesiredPosition,std::string BaseWorkspacePosition,
 		std::string BaseVelocity) {
 
 	this->desired_position_sub = nh.subscribe(DesiredPosition, 1,
 			&BaseServoController::DesiredPositionMSG, this);
 
 	this->base_velocity_pub = nh.advertise<geometry_msgs::Twist>(BaseVelocity, 2);
+	this->base_workspace_position_pub= nh.advertise<geometry_msgs::Twist>(BaseWorkspacePosition, 2);
 	this->error_update_timer = nh.createTimer(ros::Duration(0.1),
 			&BaseServoController::ErrorUpdateTimerCallback, this);
 	this->error_update_timer.stop();
@@ -81,12 +82,28 @@ void BaseServoController::DesiredPositionMSG(
 	}
 }
 
+void BaseServoController::BaseWorkspacePositionMSG(
+		const geometry_msgs::PoseStampedConstPtr& object_pose) {
+
+	tf_listener.waitForTransform("/world", object_pose->header.frame_id, object_pose->header.stamp,
+			ros::Duration(0.1));
+	tf_listener.transformPose("/world", *object_pose, this->desired_pose);
+
+	last_position_time = ros::Time().now();
+	if (error_update_timer_flag == false) {
+		this->error_update_timer.start();
+		error_update_timer_flag = true;
+
+	}
+}
+
+
 void BaseServoController::UpdateError(void) {
 	geometry_msgs::PoseStamped error_pose;
 
-	tf_listener.waitForTransform("/workspace", this->desired_pose.header.frame_id, this->desired_pose.header.stamp,
+	tf_listener.waitForTransform("/arm_mount", this->desired_pose.header.frame_id, this->desired_pose.header.stamp,
 			ros::Duration(0.1));
-	tf_listener.transformPose("/workspace", this->desired_pose, error_pose);
+	tf_listener.transformPose("/arm_mount", this->desired_pose, error_pose);
 
 
 	pos_err.x_err = error_pose.pose.position.x;
@@ -134,10 +151,11 @@ int main(int argc, char **argv) {
 	ros::NodeHandle param_nh("~");
 
 	std::string DesiredPosition("DesiredPosition"); ///String containing the topic name for goal position
-	std::string BaseVelocity("BaseVelocity"); ///String containing the topic name for BaseVelocity
+	std::string BaseWorkspacePosition("BaseWorkspacePosition"); ///String containing the topic name for BaseWorkspacePosition
+	std::string BaseVelocity("cmd_vel"); ///String containing the topic name for BaseVelocity
 
 	if (argc < 1) {
-		ROS_INFO( "Usage: base_servo_control desired_position_topic base_velocity_topic");
+		ROS_INFO( "Usage: base_servo_control desired_position_topic base_workspace_position_topic base_velocity_topic ");
 
 		return 1;
 	} else {
@@ -145,6 +163,9 @@ int main(int argc, char **argv) {
 		if (!param_nh.getParam(DesiredPosition, DesiredPosition))
 			ROS_WARN(
 					"Parameter <%s> Not Set. Using Default Desired Position Topic <%s>!", DesiredPosition.c_str(), DesiredPosition.c_str());
+		if (!param_nh.getParam(BaseWorkspacePosition, BaseWorkspacePosition))
+				ROS_WARN(
+						"Parameter <%s> Not Set. Using Default Desired Position Topic <%s>!", BaseWorkspacePosition.c_str(), BaseWorkspacePosition.c_str());
 		if (!param_nh.getParam(BaseVelocity, BaseVelocity))
 			ROS_WARN(
 					"Parameter <%s> Not Set. Using Default Base Velocity Topic <%s>!", BaseVelocity.c_str(), BaseVelocity.c_str());
@@ -152,12 +173,13 @@ int main(int argc, char **argv) {
 
 //Print out received topics
 	ROS_DEBUG("Got Desired Position Topic Name: <%s>", DesiredPosition.c_str());
+	ROS_DEBUG("Got Base Workspace Position Topic Name: <%s>", BaseWorkspacePosition.c_str());
 	ROS_DEBUG("Got Base Velocity Topic Name: <%s>", BaseVelocity.c_str());
 
 	ROS_INFO("Starting Up Base Servo Controller...");
 
 //create the arm object
-	BaseServoController base_servo(nh, DesiredPosition,  BaseVelocity);
+	BaseServoController base_servo(nh, DesiredPosition,  BaseWorkspacePosition, BaseVelocity);
 
 	ros::spin();
 }
