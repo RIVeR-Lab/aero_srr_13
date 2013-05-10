fe3ce2682d9b2b951a865014cd5c1ebdb49f6a7b
Adam Panzica <adampanzica@gmail.com>
2013-05-10 12:22:04 -0400

    Added registration for topics/parameters to MissionPlanner #106

--- aero_path_planning/include/aero_path_planning/planners/MissionPlanner.h
+++ aero_path_planning/include/aero_path_planning/planners/MissionPlanner.h
@@ -15,6 +15,8 @@
 #include <tf/tf.h>
 #include <tf/transform_listener.h>
 #include <geometry_msgs/PoseStamped.h>
+#include <aero_srr_msgs/AeroState.h>
+#include <deque>
 //************ LOCAL DEPENDANCIES ****************//
 
 //***********    NAMESPACES     ****************//
@@ -32,6 +34,90 @@ namespace aero_path_planning
 class MissionPlanner
 {
 private:
+	/**
+	 * @author Adam Panzica
+	 * @brief  Loads parameters from the ROS system
+	 */
+	void loadParam();
+
+	/**
+	 * @author Adam Panzica
+	 * @brief  Registers topics with the ROS system
+	 */
+	void registerTopics();
+
+	/**
+	 * @author Adam Panzica
+	 * @brief  Register timers with the ROS system
+	 */
+	void registerTimers();
+
+	/**
+	 * @author Adam Panzica
+	 * @brief Callback for handling changes in robot state
+	 * @param message
+	 */
+	void stateCB(const aero_srr_msgs::AeroStateConstPtr& message);
+
+	/**
+	 * @author Adam Panzica
+	 * @brief Callback for hadleing new CarrotPaths
+	 * @param message
+	 */
+	void pathCB(const nav_msgs::PathConstPtr& message);
+
+	/**
+	 * @author Adam Panzica
+	 * @brief Updates the global goal
+	 * @param event
+	 */
+	void goalCB(const ros::TimerEvent& event);
+
+	/**
+	 * @author Adam Panzica
+	 * @brief  Publishes the next goal on the carrot path
+	 */
+	void updateGoal() const;
+
+	/**
+	 * @author Adam Panzica
+	 * @brief looks up the robot's current location in the world frame
+	 * @param [in] point Point to fill with the robot's location (in meters, not grid coordinates)
+	 * @return true if the transform was sucessful, else false
+	 */
+	bool calcRobotPointWorld(app::Point& point) const;
+
+	/**
+	 * @author Adam Panzica
+	 * @brief Calculates if the next goal point in carrot path has been reached
+	 * @param [in] worldLocation The location of the robot in world coodinates (meters, not gird coordinates)
+	 * @param [in] threshold The threshold to consider the goal reached (meters, not grid coordinates)
+	 * @return True if reached, else false
+	 */
+	bool reachedNextGoal(const app::Point& worldLocation, const double threshold) const;
+
+	std::string state_topic_;           ///Topic name for receiving robot state from the supervisor
+	std::string path_topic_;            ///Topic name for receiving new carrot paths
+	std::string path_goal_topic_;       ///Topic name to publish to to update the goal used by the local planner
+	std::string mission_goal_topic_;    ///Topic name to publish noew mission goals to be used by the global planner
+	std::string global_frame_;			///frame_id of the global scale frame
+	std::string local_frame_;           ///frame_id of the local scale frame
+
+	std::deque<geometry_msgs::Pose>        mission_goals_; ///The chain of mission-goal points to fallow
+	std::deque<geometry_msgs::PoseStamped> carrot_path_;   ///The current carrot path
+
+	double                                path_threshold_;///The threshold for determining we've gotten to a point on the path, in meters
+
+	ros::NodeHandle       nh_;            ///Global NodeHandle into the ROS system
+	ros::NodeHandle       p_nh_;          ///Private NodeHandle into the ROS system
+
+	tf::TransformListener transformer_;   ///Hook into the tf system
+
+	ros::Subscriber       state_sub_;       ///Subscriber for the supervisor state
+	ros::Subscriber       path_sub_;        ///Subscriber to new carrot paths
+	ros::Publisher        path_goal_pub_;   ///Publisher to update the current global goal used by LocalPlanner
+	ros::Publisher        mission_goal_pub_;///Publisher to update the current mission goal used by GlobalPlanner
+	ros::Timer            goal_timer_;      ///Timer to update the goal point
 
 public:
 	/**
@@ -40,6 +126,7 @@ public:
 	 * @param p_nh private node handle
 	 */
 	MissionPlanner(ros::NodeHandle& nh, ros::NodeHandle& p_nh);
+
 };
 
 };
--- aero_path_planning/src/planners/MissionPlanner.cpp
+++ aero_path_planning/src/planners/MissionPlanner.cpp
@@ -11,10 +11,51 @@
 #include <aero_path_planning/planners/MissionPlanner.h>
 //***********    NAMESPACES     ****************//
 
-
 using namespace aero_path_planning;
 
-MissionPlanner::MissionPlanner(ros::NodeHandle& nh, ros::NodeHandle& p_nh)
+#define LOAD_PARAM(nh, param_name, param_store, message_stream) if(!nh.getParam(param_name, param_store)) ROS_WARN_STREAM("Parameter "<<param_name<<" not set, using default value:"<<message_stream)
+
+MissionPlanner::MissionPlanner(ros::NodeHandle& nh, ros::NodeHandle& p_nh):
+		nh_(nh),
+		p_nh_(p_nh),
+		transformer_(nh)
+{
+	ROS_INFO_STREAM("Misison Planner Starting Up...");
+	this->loadParam();
+	this->registerTopics();
+	this->registerTimers();
+	ROS_INFO_STREAM("Mission Planner Running!");
+}
+
+void MissionPlanner::loadParam()
+{
+	this->local_frame_        = "/base_footprint";
+	this->global_frame_       = "/world";
+	this->path_topic_         = "/path";
+	this->state_topic_        = "/state";
+	this->mission_goal_topic_ = "/mission_goal";
+	this->path_goal_topic_    = "/path_goal";
+	this->path_threshold_     = 1.0;
+	LOAD_PARAM(this->p_nh_, "local_frame", this->local_frame_, this->local_frame_);
+	LOAD_PARAM(this->p_nh_, "global_frame", this->global_frame_, this->global_frame_);
+	LOAD_PARAM(this->p_nh_, "state_topic", this->state_topic_, this->state_topic_);
+	LOAD_PARAM(this->p_nh_, "path_topic", this->path_topic_, this->path_topic_);
+	LOAD_PARAM(this->p_nh_, "path_threshold", this->path_threshold_, this->path_threshold_<<"m");
+	LOAD_PARAM(this->p_nh_, "mission_goal_topic", this->mission_goal_topic_, this->mission_goal_topic_);
+	LOAD_PARAM(this->p_nh_, "path_goal_topic", this->path_goal_topic_, this->path_goal_topic_);
+
+}
+
+void MissionPlanner::registerTopics()
+{
+	this->state_sub_        = this->nh_.subscribe(this->state_topic_, 1,  &MissionPlanner::stateCB, this);
+	this->path_sub_         = this->nh_.subscribe(this->path_topic_, 1, &MissionPlanner::pathCB, this);
+	this->mission_goal_pub_ = this->nh_.advertise<geometry_msgs::PoseStamped>(this->mission_goal_topic_, 1, true);
+	this->path_goal_pub_    = this->nh_.advertise<geometry_msgs::PoseStamped>(this->path_goal_topic_, 1, true);
+}
+
+void MissionPlanner::registerTimers()
 {
 
+	this->goal_timer_ = this->nh_.createTimer(ros::Duration(1.0/10.0), &MissionPlanner::goalCB, this);
 }
