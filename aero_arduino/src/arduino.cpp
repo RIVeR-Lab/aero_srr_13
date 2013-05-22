343533df429c0e45820e523ef1d0243d12dd6a22
Mitchell Wills <mwills@wpi.edu>
2013-05-22 15:40:39 -0400

    Added arduino controller to aero_base to process states for status rates and translate pause into software pause message

--- aero_arduino/src/arduino.cpp
+++ aero_arduino/src/arduino.cpp
@@ -11,13 +11,12 @@ float flash_rate = 1.0;
 void rateCb(const std_msgs::Float32& status_rate){
   flash_rate = status_rate.data;
   last_invert = 0;//make sure we make a change next time
-
 }
 
 std_msgs::Bool bool_msg;
 ros::NodeHandle nh;
-ros::Publisher pause_pub("/pause", &bool_msg);
-ros::Subscriber<std_msgs::Float32> rate_sub("/status_rate", &rateCb);
+ros::Publisher pause_pub("/arduino_pause", &bool_msg);
+ros::Subscriber<std_msgs::Float32> rate_sub("/arduino_rate", &rateCb);
 
 
 
@@ -29,19 +28,24 @@ void setup(){
   pinMode(PAUSE_PIN, INPUT);
   digitalWrite(PAUSE_PIN, HIGH);//enable pullup
   pinMode(LIGHT_PIN, OUTPUT);
+  digitalWrite(LIGHT_PIN, LOW);
 }
 
 void loop(){
   bool_msg.data = !digitalRead(PAUSE_PIN);
   pause_pub.publish( &bool_msg );
 
-  unsigned long time = millis();
-  if(time-last_invert>=(1000/flash_rate/2)){
-    digitalWrite(LIGHT_PIN, !digitalRead(LIGHT_PIN));
-    last_invert = time;
+  if(flash_rate!=0){
+    unsigned long time = millis();
+    if(time-last_invert>=(1000/flash_rate/2)){
+      digitalWrite(LIGHT_PIN, !digitalRead(LIGHT_PIN));
+      last_invert = time;
+    }
   }
+  else
+    digitalWrite(LIGHT_PIN, HIGH);
 
-  delay(100);
+  delay(50);
 
   nh.spinOnce();
 }
