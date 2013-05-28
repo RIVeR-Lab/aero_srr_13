ce2d35d9dac83616ef2734c23fb989e870470f43
Mitchell Wills <mwills@wpi.edu>
2013-05-28 15:37:13 -0400

    Made aero_description launch joint state publisher by default and not when brought up with aero_base Updated hd_driver to have more limits

--- aero_base/src/nodes/aero_camera_boom_node.cpp
+++ aero_base/src/nodes/aero_camera_boom_node.cpp
@@ -28,12 +28,22 @@ static ros::ServiceServer pose_control_srv;
 double ticks_per_radian = 4172151.34019;
 int32_t zero_tick_position = 0;
 std::string target_frame("/camera_boom_rot");
+double min_angle = -M_PI-0.1;
+double max_angle = M_PI+0.1;
 
 bool poseControlCallback(aero_base::SetBoomPosition::Request  &req,
 		     aero_base::SetBoomPosition::Response &res){
   if(req.max_velocity < 0.001){
     ROS_ERROR("Got request to go to angle with a max velocity of basically 0 (%f)", req.max_velocity);
     return false;
+  }
+  if(req.angle<min_angle){
+    ROS_WARN("Requested angle (%f) was less than min angle (%f). Limiting to min angle", req.angle, min_angle);
+    req.angle = min_angle;
+  }
+  if(req.angle>max_angle){
+    ROS_WARN("Requested angle (%f) was greater than max angle (%f). Limiting to max angle", req.angle, max_angle);
+    req.angle = max_angle;
   }
   ROS_INFO("Boom going to angle %f at max velocity %f", req.angle, req.max_velocity);
   hd_driver::SetPositionGoal hd_req;
