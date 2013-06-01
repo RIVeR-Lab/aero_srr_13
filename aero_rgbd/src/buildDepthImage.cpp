2652cb1e2ee57c450a42d47f13995f39aa2e8b3a
RJ Linton <rjlinton@wpi.edu>
2013-06-01 12:55:49 -0400

    Added subscriber, need to figure out pointcloud point data type.

--- aero_rgbd/src/buildDepthImage.cpp
+++ aero_rgbd/src/buildDepthImage.cpp
@@ -1,16 +1,28 @@
-nclude "ros/ros.h"
+#include "ros/ros.h"
+#include <sensor_msgs/PointCloud2.h>
+#include <pcl/ros/conversions.h>
+#include <pcl/point_cloud.h>
+#include <pcl/point_types.h>
 #include "std_msgs/String.h"
 #include <sstream>
 
-int main(int argc, char **argv)
+//Need to figure out what point type Samir is using.
+void pointCloud_callBack( const pcl::PointCloud<T> input )
 {
 
-    ros::init(argc, argv, "buildDepthImage");
+}
 
+int main(int argc, char **argv)
+{
+    //Initialize, and create a node handle
+    ros::init(argc, argv, "buildDepthImage");
     ros::NodeHandle n;
 
+    //Create subscriber for the PointCloud2
+    ros::Subscriber sub = n.subscribe( "input", 1, pointCloud_callBack );
+
     //Just publishing junk message until I get the image message code written.
-    ros::Publisher depthImage_publisher = n.advertise<std_msgs::String>("Someday I'll be an image", 1000);
+    ros::Publisher depthImage_publisher = n.advertise<std_msgs::String>("Someday_I_ll_be_an_image", 1000);
 
     ros::Rate loop_rate(10);
 
