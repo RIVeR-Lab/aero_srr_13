177e1a5568f6c1407f53280d81d29b6bdf457452
RJ Linton <rjlinton@wpi.edu>
2013-06-01 16:33:32 -0400

    Finished building the depth image builder node.

--- aero_rgbd/src/buildDepthImage.cpp
+++ aero_rgbd/src/buildDepthImage.cpp
@@ -1,28 +1,73 @@
 #include "ros/ros.h"
+#include <image_transport/image_transport.h>
+#include <sensor_msgs/image_encodings.h>
 #include <sensor_msgs/PointCloud2.h>
 #include <pcl/ros/conversions.h>
 #include <pcl/point_cloud.h>
+#include <pcl_ros/point_cloud.h>
 #include <pcl/point_types.h>
 #include "std_msgs/String.h"
 #include <sstream>
 
-//Need to figure out what point type Samir is using.
-void pointCloud_callBack( const pcl::PointCloud<T> input )
-{
+class DepthImageBuilder {
 
-}
+    private:
+        sensor_msgs::Image depthImageMsg;
+
+        template<typename CloudT> void
+        toROSDepthMsg( const CloudT cloud )
+        {
+            //Test to make sure the cloud is dense.
+            if(cloud->width == 0 && cloud->height == 0 )
+                throw std::runtime_error("Needs to be a dense cloud.");
+            else
+            {
+                if(cloud->points.size() != cloud->width * cloud->height)
+                    throw std::runtime_error("The width and height do not match the cloud size.");
+                //Set the dimensions of the depthImage
+                depthImageMsg.height = cloud->height;
+                depthImageMsg.width = cloud->width;
+            }
+            //The image is the Z coordinates from the cloud frame, i.e. the depths in mm as 16bit unsigned ints
+            depthImageMsg.encoding = sensor_msgs::image_encodings::TYPE_8UC1;
+            depthImageMsg.step = depthImageMsg.width * sizeof(uint8_t);
+            depthImageMsg.data.resize( depthImageMsg.step * depthImageMsg.height );
+
+            //Copy data from the cloud to the image.
+            for( size_t y = 0; y < cloud->height; y++ )
+            {
+                for( size_t x = 0; x < cloud->width; x++ )
+                {
+                    uint8_t *pixel = &( depthImageMsg.data[ y * depthImageMsg.step + x ] );
+                    memcpy( pixel, &cloud->at(x, y).z, sizeof(uint8_t));
+                }
+            }
+        }
+
+    public:
+        void pointCloud_callback( const pcl::PointCloud<pcl::PointXYZRGB>::ConstPtr& input )
+        {
+            toROSDepthMsg( input );
+        }
+
+        sensor_msgs::Image getDepthImageMsg(){return depthImageMsg;}
+};
 
 int main(int argc, char **argv)
 {
+    DepthImageBuilder builder;
+
     //Initialize, and create a node handle
     ros::init(argc, argv, "buildDepthImage");
     ros::NodeHandle n;
 
     //Create subscriber for the PointCloud2
-    ros::Subscriber sub = n.subscribe( "input", 1, pointCloud_callBack );
+    ros::Subscriber sub = n.subscribe<pcl::PointCloud<pcl::PointXYZRGB> >( "input", 1, &DepthImageBuilder::pointCloud_callback, &builder );
 
     //Just publishing junk message until I get the image message code written.
-    ros::Publisher depthImage_publisher = n.advertise<std_msgs::String>("Someday_I_ll_be_an_image", 1000);
+    //Setup image transport and publish the image message.
+    image_transport::ImageTransport it(n);
+    image_transport::Publisher imagePublisher = it.advertise("/upper_stereo/depth_image", 1);
 
     ros::Rate loop_rate(10);
 
@@ -37,7 +82,7 @@ int main(int argc, char **argv)
 
         ROS_INFO("%s", msg.data.c_str());
 
-        depthImage_publisher.publish(msg);
+        imagePublisher.publish(builder.getDepthImageMsg());
 
         ros::spinOnce();
 
